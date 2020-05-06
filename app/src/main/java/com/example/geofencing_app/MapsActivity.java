918b8b37f8e3126b004c37ebe2757fc332c54200
Eric Njuguna <eric7njuguna@gmail.com>
2020-05-06 23:09:15 +0300

    Added firebase integration to the network lists

--- app/src/main/java/com/example/geofencing_app/MapsActivity.java
+++ app/src/main/java/com/example/geofencing_app/MapsActivity.java
@@ -1,12 +1,26 @@
 package com.example.geofencing_app;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.widget.Toast;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -16,15 +30,26 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,6 +58,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationCallback locationCallback;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private Marker currentUser;
+    private DatabaseReference myLocationRef;
+    private GeoFire geoFire;
+    private List<LatLng> advertisingArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +79,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                                 .findFragmentById(R.id.map);
                         mapFragment.getMapAsync(MapsActivity.this);
+
+                        settingGeoFire();
+                        initArea();
                     }
 
                     @Override
@@ -69,19 +100,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    private void initArea() {
+        advertisingArea = new ArrayList<>();
+        advertisingArea.add(new LatLng(-1.2891,  36.8266));
+        advertisingArea.add(new LatLng(-0.2850, 36.0693));
+        advertisingArea.add(new LatLng(-0.0982, 34.7620));
+
+        FirebaseDatabase.getInstance()
+                .getReference("AdvertisingArea")
+                .push()
+                .setValue(advertisingArea)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MapsActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MapsActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void settingGeoFire() {
+    myLocationRef = FirebaseDatabase.getInstance().getReference("MyLocation");
+    geoFire = new GeoFire(myLocationRef);
+
+    }
+
     private void buildLocationCallback() {
     locationCallback = new LocationCallback(){
         @Override
-        public void onLocationResult(LocationResult locationResult) {
+        public void onLocationResult(final LocationResult locationResult) {
            if(mMap != null){
-               if(currentUser != null) currentUser.remove();
-               currentUser = mMap.addMarker(new MarkerOptions()
-               .position(new LatLng(locationResult.getLastLocation().getLatitude(),
-                       locationResult.getLastLocation().getLongitude()))
-               .title("You"));
-               //After adding the Marker, Move the Camera
-               mMap.animateCamera(CameraUpdateFactory
-               .newLatLngZoom(currentUser.getPosition(), 12.0f));
+
+
+               geoFire.setLocation("You", new GeoLocation(locationResult.getLastLocation().getLatitude(),
+                       locationResult.getLastLocation().getLongitude()), new GeoFire.CompletionListener() {
+                   @Override
+                   public void onComplete(String key, DatabaseError error) {
+                       if(currentUser != null) currentUser.remove();
+                       currentUser = mMap.addMarker(new MarkerOptions()
+                               .position(new LatLng(locationResult.getLastLocation().getLatitude(),
+                                       locationResult.getLastLocation().getLongitude()))
+                               .title("You"));
+                       //After adding the Marker, Move the Camera
+                       mMap.animateCamera(CameraUpdateFactory
+                               .newLatLngZoom(currentUser.getPosition(), 12.0f));
+                   }
+               });
            }
         }
     };
@@ -106,11 +175,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
       if (fusedLocationProviderClient != null)
           fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+
+      //Add a Circle for the Advertising Area
+        for (LatLng latLng: advertisingArea){
+            mMap.addCircle(new CircleOptions().center(latLng)
+            .radius(500) //500 Meters
+            .strokeColor(Color.BLUE)
+            .fillColor(0x220000FF)//22 is a transparent Code
+            .strokeWidth(5.0f)
+            );
+
+       //Create a GeoQuery when the user is in the Advertising Location
+            GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(latLng.latitude, latLng.longitude) ,0.5f);//300m
+            geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+                @Override
+                public void onKeyEntered(String key, GeoLocation location) {
+                    sendNotification("Geofence", String.format(" %s You have Entered the Advertising area", key));
+                    Toast.makeText(MapsActivity.this, "You have Entered the Advertising Area", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onKeyExited(String key) {
+                    sendNotification("Geofence", String.format(" %s You have Left the Advertising area", key));
+                    Toast.makeText(MapsActivity.this, "You have left the Advertising Area", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onKeyMoved(String key, GeoLocation location) {
+                  //  sendNotification("Geofence", String.format(" %s You are moving within Advertising area", key));
+                }
+
+                @Override
+                public void onGeoQueryReady() {
+
+                }
+
+                @Override
+                public void onGeoQueryError(DatabaseError error) {
+                    Toast.makeText(MapsActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void sendNotification(String title, String content) {
+        String NOTIFICATION_CHANNEL_ID = "edmt_multiple_location";
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            //config
+            notificationChannel.setDescription("Channel Description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        builder.setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(false)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+
+        Notification notification = builder.build();
+        notificationManager.notify(new Random().nextInt(), notification);
+
     }
 
     @Override
     protected void onStop() {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback);
         super.onStop();
+
+
     }
-}
+
+
+
+    }
+
+
